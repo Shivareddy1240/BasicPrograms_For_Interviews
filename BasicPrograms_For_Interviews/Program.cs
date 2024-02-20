a3c400cee7ce3798f18e58db855ee76260ea7c5e
Shivareddy Mudireddy <shivamudhireddy@gmail.com>
2024-02-20 19:55:28 +0530

    Add project files.

---
+++ BasicPrograms_For_Interviews/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using BasicPrograms_For_Interviews;
+
+namespace BasicPrograms;
+
+public class Program
+{
+   
+    static void Main(string[] args)
+    {
+        StringReverse stringReverse = new StringReverse();
+        Console.WriteLine("Please enter a string to reverse");
+        string input = Console.ReadLine();
+        stringReverse.ReverseUsingStack(input);
+        Console.WriteLine("Please enter a string to reverse");
+        string input2 = Console.ReadLine();
+        stringReverse.ReverseStringForLoop(input2);
+        Console.WriteLine("Please enter a string to reverse");
+        string input3 = Console.ReadLine();
+        stringReverse.ReverseStringWhileLoop(input3);
+    }
+    
+}
---
+++ BasicPrograms_For_Interviews/StringReverse.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms_For_Interviews
+{
+    public class StringReverse
+    {
+        //String Reverse using for loop
+        public void ReverseStringForLoop(string name)
+        {
+            char[] chars = name.ToCharArray();
+            string reversedString = string.Empty;
+            for (int i = chars.Length - 1; i > -1; i--)
+            {
+                reversedString += chars[i];
+            }
+           Console.WriteLine(reversedString);
+        }
+
+        public void ReverseStringWhileLoop(string name)
+        {
+            char[] chars = name.ToCharArray();
+            int start = 0;
+            int end = name.Length - 1;
+
+            while(start < end)
+            {
+                char temp = chars[start];
+                chars[start] = chars[end];
+                chars[end] = temp;
+
+                start++;
+                end--;
+            }
+            Console.WriteLine(chars);
+        }
+
+        //String Reverse Using Stack
+        public void ReverseUsingStack(string name)
+        {
+            char[] chars = name.ToCharArray();
+            Stack stack = new Stack();
+            foreach (char ch in chars)
+            {
+                stack.Push(ch.ToString());
+            }
+
+            string reversedString = string.Empty;
+            while (stack.Count > 0)
+            {
+                reversedString += stack.Pop();
+            }
+
+            Console.WriteLine(reversedString);
+        }
+
+    }
+}
